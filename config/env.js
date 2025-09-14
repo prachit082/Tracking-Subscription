e9d0747ed1b9c472174e83c536f5fb5bdf28a4f4
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-09-14 12:15:29 +0530

    MongoDB Setup

--- config/env.js
+++ config/env.js
@@ -3,10 +3,5 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
-  PORT, NODE_ENV, SERVER_URL,
-  DB_URI,
-  JWT_SECRET, JWT_EXPIRES_IN,
-  ARCJET_ENV, ARCJET_KEY,
-  QSTASH_TOKEN, QSTASH_URL,
-  EMAIL_PASSWORD,
+  PORT, NODE_ENV, DB_URI
 } = process.env;
